03fd46887b6f64017e0b60eae22789dfcd80b138
Juan Daniel Galarza <juan.galarza@correounivalle.edu.co>
2018-07-04 22:41:52 -0500

    added upcoming movies component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,12 +6,14 @@ import {MovieListTopRatedComponent} from './components/Movie/movie-list-top-rate
 import {ListPopularPeopleComponent} from './components/People/list-popular-people/list-popular-people.component';
 import {PersonDetailComponent} from './components/People/person-detail/person-detail.component';
 import {MovieListNowPlayingComponent} from './components/Movie/movie-list-now-playing/movie-list-now-playing.component';
+import {MovieListUpcomingComponent} from './components/Movie/movie-list-upcoming/movie-list-upcoming.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/list-movies/popular/1', pathMatch: 'full' },
   { path: 'list-movies/popular/:page', component: MovieListComponent },
   { path: 'list-movies/top_rated/:page', component: MovieListTopRatedComponent },
   { path: 'list-movies/now_playing/:page', component: MovieListNowPlayingComponent },
+  { path: 'list-movies/upcoming/:page', component: MovieListUpcomingComponent },
   { path: 'people/popular/:page', component: ListPopularPeopleComponent },
   { path: 'movie/:id', component: MovieDetailsComponent },
   { path: 'person/:id', component: PersonDetailComponent },
--- src/app/components/Movie/movie-list-upcoming/movie-list-upcoming.component.ts
+++ src/app/components/Movie/movie-list-upcoming/movie-list-upcoming.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+import {IPageChangeEvent} from '@covalent/core/paging';
+import {MovieService} from '../../../services/movie.service';
 
 @Component({
   selector: 'app-movie-list-upcoming',
@@ -7,9 +10,81 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MovieListUpcomingComponent implements OnInit {
 
-  constructor() { }
+  // Attributes
+  eventLinks: IPageChangeEvent;
+  movies: any[];
+  page = 1;
+  url_image = 'https://image.tmdb.org/t/p/w500';
+  // used for responsive
+  breakpoint;
+
+  // Used for pagination
+  currentPage = 1;
+  firstLast = true;
+  totalResults: number;
+  totalPages: number;
+
+  constructor(
+    private movieService: MovieService,
+    public router: Router,
+    private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.currentPage = +this.route.snapshot.paramMap.get('page');
+    this.getMoviesActualPage();
+    this.breakpoint = (window.innerHeight <= 400) ? 1 : 4;
+  }
+
+  /**
+   * manages the behavior of the pagination
+   * @param event: Event of change the page
+   */
+  changePage(event: IPageChangeEvent): void {
+    this.currentPage = event.page;
+    this.router.navigate(['/list-movies/upcoming', this.currentPage]);
+    this.getMoviesActualPage();
+
+  }
+
+  // Method for get the movies with the actual page
+  getMoviesActualPage() {
+    this.movieService.getUpcomingMovies(this.currentPage).subscribe(
+      (data: any ) => {
+        this.movies = data;
+        this.totalResults = this.movies[0].total_results;
+        this.totalPages = this.movies[0].total_pages;
+      },
+      (error: any) => {
+        console.log(error);
+      }
+    );
+  }
+
+  /**
+   * Methods for manage the behavior of the screen
+   * @param event: Event of change the page size
+   */
+  onResize(event) {
+    if ((event.target.innerWidth <= 400) && (event.target.innerWidth > 0)) {
+      this.breakpoint = 1;
+    } else if ((event.target.innerWidth <= 800) && (event.target.innerWidth > 400)) {
+      this.breakpoint = 2;
+    } else if ((event.target.innerWidth <= 1200) && (event.target.innerWidth > 800)) {
+      this.breakpoint = 3;
+    } else if ((event.target.innerWidth <= 1400) && (event.target.innerWidth > 1200)) {
+      this.breakpoint = 4;
+    }
+  }
+
+  /**
+   * manages the pagination
+   * @param event: Event of change the page
+   */
+  changeLinks(event: IPageChangeEvent): void {
+    this.eventLinks = event;
+    this.page = this.eventLinks.page;
+    this.router.navigate(['/list-movies/upcoming', this.page]);
+    this.getMoviesActualPage();
   }
 
 }
